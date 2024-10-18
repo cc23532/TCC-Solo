11e8dee649e0ac49ff1cedc03784b6f82b35682e
Davi Piovezana Murata <cc23509@g.unicamp.br>
2024-10-17 21:33:13 -0300

    Tela de Adicionar alimentos na dieta

---
+++ teste/solo/app/src/main/java/com/example/solo/DietSection/DietNewActivity.java
@@ -0,0 +1,26 @@
+package com.example.solo.DietSection;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.solo.R;
+
+public class DietNewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_diet_new);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
