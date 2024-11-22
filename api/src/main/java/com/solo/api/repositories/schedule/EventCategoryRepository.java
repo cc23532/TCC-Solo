b3d7d1184f878d12e61fc47d2647d54b8747305a
cc23532 <cc23532@g.unicamp.br>
2024-11-21 22:39:24 -0300

    APIs: agenda

--- api/src/main/java/com/solo/api/repositories/schedule/EventCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.solo.api.repositories.schedule;
-
-import com.solo.api.models.schedule.EventCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventCategoryRepository extends JpaRepository<EventCategory, Integer> {
-}
