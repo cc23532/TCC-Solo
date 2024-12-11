b606642fdfd387a6019d5d802070a3d1cd08ef4f
matheuscarletti03 <cc23532@g.unicamp.br>
2024-12-10 21:19:10 -0300

    API: GetMoodCountByTimeFrame concluído

--- api/src/main/java/com/solo/api/controllers/user/UserMoodController.java
+++ api/src/main/java/com/solo/api/controllers/user/UserMoodController.java
@@ -2,12 +2,11 @@ package com.solo.api.controllers.user;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import com.solo.api.models.user.SoloUser;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+
 @RestController
 @RequestMapping("/user/mood")
 public class UserMoodController {
@@ -32,6 +32,33 @@ public class UserMoodController {
     public List<UserMood> getMoodById(@PathVariable Integer idUser) {
         return repository.findMoodByIdUser(idUser);
     }
+
+    @GetMapping("/count/{idUser}")
+    public ResponseEntity<?> getMoodCountByTimeFrame(@PathVariable Integer idUser, @RequestBody Map<String, String> body) {
+        String timeFrame = body.get("timeFrame");
+        List<Object[]> results = repository.getMoodCountByTimeFrame(timeFrame, idUser);
+        Map<String, Object> response = new HashMap<>();
+    
+        // Inicializar os counts com zero
+        response.put("idUser", idUser);
+        response.put("veryhappy", 0);
+        response.put("happy", 0);
+        response.put("normal", 0);
+        response.put("unhappy", 0);
+        response.put("sad", 0);
+    
+        // Preencher os moods existentes no resultado
+        for (Object[] result : results) {
+            String mood = (String) result[0]; // Nome do humor
+            Integer count = (Integer) result[1];   // Quantidade de ocorrências
+            if (response.containsKey(mood)) {
+                response.put(mood, count != null ? count : 0);
+            }
+        }
+    
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+     
 
     @PostMapping("/{idUser}")
     public ResponseEntity<?> registerMood(@PathVariable SoloUser idUser, @RequestBody Map<String, String> body) {
--- api/src/main/java/com/solo/api/repositories/user/UserMoodRepository.java
+++ api/src/main/java/com/solo/api/repositories/user/UserMoodRepository.java
@@ -1,20 +1,21 @@
 package com.solo.api.repositories.user;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.solo.api.models.user.UserMood;
 
-import jakarta.transaction.Transactional;
-
-import java.util.Date;
-import java.util.List;
 
 public interface UserMoodRepository extends JpaRepository<UserMood, Integer>{
 
     @Query(value = "SELECT * FROM appSolo.UserMood WHERE idUser = :idUser", nativeQuery = true)
     public List<UserMood> findMoodByIdUser(@Param("idUser") Integer idUser);
                             
+    @Query(value = "EXEC appsolo.GetMoodCountByTimeFrame :timeFrame, :idUser", nativeQuery = true)
+    public List<Object[]> getMoodCountByTimeFrame(  @Param("timeFrame") String timeFrame,
+                                                    @Param("idUser") Integer idUser);
+
 }
